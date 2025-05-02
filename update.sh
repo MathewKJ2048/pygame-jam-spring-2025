d5c4aa881b139f8620cac9c3d8f6f492144108b3
Mathew Kuthur James <mathewkj2048@gmail.com>
2025-05-02 11:45:25 -0400

    remote trigger update

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get the directory where the script is being executed from
+CWD="$(pwd)"
+
 # Check if a commit message is provided
 if [ -z "$1" ]; then
     echo "Error: Please provide a commit message as the first argument."
@@ -9,7 +12,7 @@ fi
 
 commit_message="$*"
 
-# Git operations
-git add .
-git commit -m "$commit_message"
-git push
+# Git operations (using -C to specify the working directory)
+git -C "$CWD" add .
+git -C "$CWD" commit -m "$commit_message"
+git -C "$CWD" push
